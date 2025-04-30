b3d922d39e05f4a909c7f23c4646071a7d31f201
guka20 <gukadavitadze23@gmail.com>
2025-04-30 20:55:12 +0400

    Trending and New Arrival done

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,35 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { getAllProducts } from "@/helper/api";
-import { CartItem } from "@/components";
-import ProductsLayout from "@/Layouts/ProductsLayout/ProductsLayout";
+import React from "react";
 import "./Home.css";
+import TrendingProducts from "./components/TrendingProducts/TrendingProducts";
+import NewProducts from "./components/NewProducts/NewProducts";
+
 const Home = () => {
-  const [products, setProducts] = useState();
-  
-  useEffect(() => {
-    getAllProducts(0,8).then((resp) => setProducts(resp.products));
-  }, []);
-  if (products?.length === 0) {
-    return <h1>There are no any products</h1>;
-  }
   return (
     <div className="home-page">
-      {products === undefined ? (
-        <h1>Loading...</h1>
-      ) : (
-        <ProductsLayout title="Trending Products">
-          {products.map((el) => (
-            <CartItem
-              id={el.id}
-              title={el.title}
-              category={el.category}
-              thumbnail={el.thumbnail}
-              price={el.price}
-              discountPercentage={el.discountPercentage}
-            />
-          ))}
-        </ProductsLayout>
-      )}
+      <TrendingProducts />
+      <br />
+      <hr />
+      <br />
+      <NewProducts />
     </div>
   );
 };
--- src/helper/api.js
+++ src/helper/api.js
@@ -1,4 +1,4 @@
-import axios from "../config/axios";
+import axios from "@/config/axios";
 
 async function getAllProducts(skip, limit) {
   const resp = await axios.get("products", {
