10660d72676ed894d3a434ededcec23d44da9a66
guka20 <gukadavitadze23@gmail.com>
2025-05-07 21:31:05 +0400

    footer is done

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import "./Footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <p>© Copyright | All Rights Reserved</p>
+      <a href="#">Next Step Academy</a>
+    </div>
+  );
 };
 
 export default Footer;
