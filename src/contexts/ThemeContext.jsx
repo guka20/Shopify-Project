e960dfcb0313b7afb82a2182fbcb22273fa898ba
guka20 <gukadavitadze23@gmail.com>
2025-05-30 21:16:51 +0400

    darktheme localstorage

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -1,9 +1,17 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const ThemeContext = createContext();
 
 const ThemeContextProvider = ({ children }) => {
-  const [isDark, setIsDark] = useState(false);
+
+
+  let isDarkLoc = JSON.parse(localStorage.getItem("isDark")) || false;
+  const [isDark, setIsDark] = useState(isDarkLoc);
+
+  useEffect(() => {
+    localStorage.setItem("isDark", isDark);
+  }, [isDark]);
+
   const obj = { isDark, setIsDark };
   return <ThemeContext.Provider value={obj}>{children}</ThemeContext.Provider>;
 };
