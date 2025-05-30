7f0900eaf8db5988d9827438cd429736ef555a46
guka20 <gukadavitadze23@gmail.com>
2025-05-30 20:34:51 +0400

    Authentication is done

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 import CartItem from "./CartItem/CartItem";
-export { Navbar, Footer, CartItem };
+import Loading from "./Loading/Loading";
+export { Navbar, Footer, CartItem, Loading };
--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,12 +1,30 @@
-import React, { createContext, useState } from "react";
+import { Loading } from "@/components";
+import { loginUsingToken } from "@/helper/api";
+import React, { createContext, useEffect, useState } from "react";
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
   const [userAuth, setUserAuth] = useState(null);
-  
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    let accToken = localStorage.getItem("accToken");
+    if (accToken === null) {
+      setIsLoading(false);
+    } else {
+      loginUsingToken(accToken)
+        .then((resp) => setUserAuth(resp))
+        .catch(() => {
+          localStorage.removeItem("accToken");
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    }
+  }, []);
   return (
     <AuthContext.Provider value={{ userAuth, setUserAuth }}>
+      {isLoading ? <Loading /> : null}
       {children}
     </AuthContext.Provider>
   );
